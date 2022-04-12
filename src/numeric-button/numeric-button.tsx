2346dfe61c70cbb69d12b571af7a2ed8cb11bb1a
mikawix <mikan@wix.com>
2022-04-12 22:13:28 +0300

    m

--- src/numeric-button/numeric-button.tsx
+++ src/numeric-button/numeric-button.tsx
@@ -2,5 +2,16 @@ import React from 'react';
 import './numeric-button.scss';
 
 export const NumericButton: React.FC = () => {
-    return <div className='numeric-button__root'>NumericButton</div>;
+    return <div className='numeric-button__root'>
+        <div className="buttonRow">
+            <div className="one numeric">1</div>
+            <div className="two numeric">2</div></div>
+        <div className="buttonRow">
+            <div className="three numeric">3</div>
+            <div className="four numeric">4</div>
+        </div>
+        <div className="buttonRow">
+            <div />
+        </div>
+        <div className="buttonRow" /></div>;
 };
