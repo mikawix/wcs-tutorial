1ca2722dcff8c1b91e93eeb9c609dc0cff774494
mikawix <mikan@wix.com>
2022-04-14 10:20:04 +0300

    outline

--- src/spacing/spacing.tsx
+++ src/spacing/spacing.tsx
@@ -4,5 +4,12 @@ import { MedButton } from '../med-button/med-button';
 
 export const Spacing: React.FC = () => {
     return <div className='spacing__root'>
+        <div className="spaceRow">
+            <div className="circle">N</div>
+            <div className="circle">I</div>
+            <div className="circle">C</div>
+            <div className="circle">E</div>
+        </div>
+        <div className="line" />
         <div className="giveSpace">Give Me Some Space</div></div>;
 };
