e06c445fe1ce14368facd3c9f2b22d73ab297f05
mikawix <mikan@wix.com>
2022-04-14 16:31:33 +0300

    we

--- src/numeric-button/numeric-button.tsx
+++ src/numeric-button/numeric-button.tsx
@@ -7,10 +7,6 @@ export const NumericButton: React.FC = () => {
             <div className="one numeric">1</div>
             <div className="two numeric">2</div></div>
         <div className="buttonRow">
-            <div className="three numeric">3</div>
-            <div className="four numeric">4</div>
-        </div>
-        <div className="buttonRow">
             <div className="five numeric">5</div>
         </div>
         <div className="buttonRow">
@@ -18,6 +14,9 @@ export const NumericButton: React.FC = () => {
             <div className="seven numeric">7</div>
             <div className="eight numeric">8</div>
         </div>
-    </div>
+        <div className="buttonRow">
+            <div className="three numeric">3</div>
+            <div className="four numeric">4</div>
+        </div></div>
     </div>;
 };
