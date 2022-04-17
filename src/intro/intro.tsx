164340934d7a556b93f356016704b83f7586cd2f
mikawix <mikan@wix.com>
2022-04-17 22:45:23 +0300

    g

--- src/intro/intro.tsx
+++ src/intro/intro.tsx
@@ -5,7 +5,6 @@ import welcome from '../assets/welcome.png';
 export const Intro: React.FC = () => {
     return (
         <div className='intro'>
-            <img src={welcome} />
-            <div className="flexRow"></div></div>
+            <img src={welcome} /></div>
     )
 };
