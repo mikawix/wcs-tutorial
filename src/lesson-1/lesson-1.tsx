7c529fc8ad76e16ae2d4fc0d7a14214296a818da
mikawix <mikan@wix.com>
2022-04-17 21:54:51 +0300

    b

--- src/lesson-1/lesson-1.tsx
+++ src/lesson-1/lesson-1.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './lesson-1.scss';
+import { Intro } from '../intro/intro';
 
 export const Lesson1: React.FC = () => {
     return <div className='lesson-1__root'>
         <div className="lesson1Desc">
-            Lesson1</div>
-        <div /></div>;
+            <div className="l1progress">01/09</div>
+            <div className="l1title">
+                <h1>Intro to WCS</h1>
+            </div>
+            <div className="l1task">
+                <li>This is a board. To edit or style elements, select them on stage or on the layer panel to the left.		</li>
+                <li>When element is selected, you can inspect or edit its style, or change properties on the right panel.</li>
+            </div>
+        </div>
+        <div className="lesson1Card">
+            <Intro />
+        </div></div>;
 };
