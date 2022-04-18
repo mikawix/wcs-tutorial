66e447429fe589d227930cc956420b2d48b54ec2
mikawix <mikan@wix.com>
2022-04-18 10:18:55 +0300

    recClass

--- src/intro-rectangle/intro-rectangle.tsx
+++ src/intro-rectangle/intro-rectangle.tsx
@@ -1,6 +1,33 @@
 import React from 'react';
 import './intro-rectangle.scss';
 
-export const IntroRectangle: React.FC = () => {
-    return <div className="rectangle"></div>;
+export interface IntroRectabngleProps {
+    size?: 'small' | 'medium' | 'large';
+    shape?: 'circle' | 'ellipse';
+}
+
+export const IntroRectangle: React.FC<IntroRectabngleProps> = ({
+    size = 'small',
+    shape = 'circle'
+
+}) => {
+    let recClass: string = 'rectangle';
+    switch (size) {
+        case 'small':
+            recClass = shape === 'circle' ? 'smallCircle' : 'smallEllipse'
+            break;
+        case 'medium':
+            recClass = shape === 'circle' ? 'medCircle' : 'medEllipse'
+            break;
+        case 'large':
+            recClass = shape === 'circle' ? 'largeCircle' : 'largeEllipse'
+            break;
+        default:
+            recClass = 'rectangle'
+            break;
+    }
+    
+    return (
+        <div className={recClass}></div>
+    );
 };
