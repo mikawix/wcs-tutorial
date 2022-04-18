5971a20172571b9c71343b8a084823b89f9948d4
mikawix <mikan@wix.com>
2022-04-18 17:38:29 +0300

    y

--- src/intro/intro.tsx
+++ src/intro/intro.tsx
@@ -7,7 +7,7 @@ export const Intro: React.FC = () => {
     return (
         <div className='intro'>
             <img src={welcome} />
-            <IntroRectangle shape="ellipse" size="large" angle={-37} backgroundColor="#B6C1F0" left={1} top={52} zIndex={1} />
+            <IntroRectangle shape="ellipse" size="medium" angle={-37} backgroundColor="#B6C1F0" left={1} top={52} zIndex={1} />
             <IntroRectangle shape="ellipse" size="medium" angle={5} backgroundColor="#92D0AB" left={23} top={51} zIndex={0} />
             <IntroRectangle shape="ellipse" size="small" angle={-62} backgroundColor="#DCD9BF" left={122} top={95} zIndex={1} />
             <IntroRectangle shape="circle" size="medium" angle={18} backgroundColor="#7FC2F4" left={219.37} top={64} />
@@ -16,9 +16,15 @@ export const Intro: React.FC = () => {
             <IntroRectangle shape="ellipse" size="medium" angle={-17} backgroundColor="#7FC2F4" left={24} top={166} />
             <IntroRectangle shape="circle" size="large" backgroundColor="#FFB615" left={99} top={140} zIndex={2} />
             <IntroRectangle shape="ellipse" size="medium" backgroundColor="#FF5B2B" left={192} top={148} zIndex={1} />
-            <IntroRectangle backgroundColor="#268040" angle={38} zIndex={1} top={228} left={28} size="medium" shape="circle" />
+            <IntroRectangle backgroundColor="#268040" angle={38} zIndex={0} top={228} left={48} size="medium" shape="circle" />
             <IntroRectangle backgroundColor="#A09D2A" left={180} top={228} size="medium" shape="circle" zIndex={2} />
             <IntroRectangle backgroundColor="#6A55A2" left={235} top={208} shape="ellipse" size="small" angle={-17} />
+            <IntroRectangle backgroundColor="#FF5B2B" left={26} top={270} size="medium" shape="ellipse" angle={17} zIndex={2} />
+            <IntroRectangle backgroundColor="#FA9DBC" left={38} top={300} shape="ellipse" size="medium" angle={-17} zIndex={1} />
+            <IntroRectangle backgroundColor="#DCD9BF" shape="ellipse" size="large" left={114} top={310} angle={-28} />
+            <IntroRectangle backgroundColor="#268040" shape="ellipse" size="medium" left={230} top={370} angle={54} zIndex={2} />
+            <IntroRectangle backgroundColor="#90D3AA" left={176} shape="ellipse" size="medium" top={392} angle={18} />
+            <IntroRectangle backgroundColor="#FF5B2B" size="small" shape="circle" left={230} top={367} zIndex={1} />
         </div>
     )
 };
